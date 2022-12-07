1c0dd48d89ce47fefc9d0f34c1b93e0f0aa9a189
Claire Smith <claire.smith@yapily.com>
2022-12-07 23:22:19 +0000

    day 3 part 2

--- day3.js
+++ day3.js
@@ -1,26 +1,26 @@
+const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
 const getData = () => {
     let data = require("fs").readFileSync("day3.txt", 'utf8')
     data = data.split('\n')
     return data
 }
 
-const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-
-const traverseData = () => {
+const traverseDataPartOne = (func) => {
     let sum = 0
     const rucksackData = getData()
     for (let i = 0; i < rucksackData.length; i++) {
-       sum += findCommonCharacters(rucksackData, i)
+       sum += func(rucksackData, i)
         
     }
     return sum
 }
 
-const findCommonCharacters = (rucksackData, i) => {
+const findCommonCharactersPartOne = (rucksackData, i) => {
     const bothCompartments = rucksackData[i].length
+    const secondCompartment = rucksackData[i].slice(bothCompartments / 2, bothCompartments)
     for (let j = 0; j < (bothCompartments) / 2; j++) {
         const currentChar = rucksackData[i][j]
-        const secondCompartment = rucksackData[i].slice(bothCompartments / 2, bothCompartments)
 
         for (let k = 0; k < secondCompartment.length; k++) {
             if (currentChar === secondCompartment[k]) {
@@ -31,4 +31,29 @@ const findCommonCharacters = (rucksackData, i) => {
     }
 }
 
-traverseData()
+traverseDataPartOne(findCommonCharactersPartOne)
+
+const traverseDataPartTwo = (func) => {
+    let sum = 0
+    let subArraySize = 3
+    let rucksackData = getData()
+    for (let i = 0; i < rucksackData.length; i += subArraySize)  {
+        sum += func(rucksackData.slice(i, i + subArraySize), i)
+    }
+    return sum
+}
+
+const findCommonCharactersPartTwo = (group, i) => {
+
+    for (let i = 0; i < group[0].length; i++) {
+        for (let j = 0; j < group[1].length; j++) {
+            for (let k = 0; k < group[2].length; k++) {
+                if (group[0][i] === group[1][j] && group[1][j] === group[2][k]) {
+                    return characters.indexOf(group[0][i]) + 1
+                }
+            }
+        }  
+    }
+
+}
+traverseDataPartTwo(findCommonCharactersPartTwo)
